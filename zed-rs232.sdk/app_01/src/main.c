a1f29f5d0b550ba0638500510f00caa411ed58e4
meysam <mysm.shahbazi@gmail.com>
2021-03-04 23:21:51 +0330

    set stdin to uart1 in bsp setting

---
+++ zed-rs232.sdk/app_01/src/main.c
@@ -0,0 +1,22 @@
+/*
+ * main.c
+ *
+ *  Created on: Mar 4, 2021
+ *      Author: meysam
+ */
+
+#include "xparameters.h"
+
+int main() {
+
+
+
+	while(1){
+		xil_printf("im here\r\n");
+	}
+	return 0;
+}
+
+
+
+
