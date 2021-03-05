f5c3be145858b7c57b094fc85c787ae8117b4a02
meysam <mysm.shahbazi@gmail.com>
2021-03-05 13:30:00 +0330

    uart in poll mode is ok

--- zed-rs232.sdk/app_01/src/main.c
+++ zed-rs232.sdk/app_01/src/main.c
@@ -5,52 +5,27 @@
  *      Author: meysam
  */
 
-
 #include "xparameters.h"
-#include "xplatform_info.h"
 #include "xuartps.h"
-#include "xil_exception.h"
 #include "xil_printf.h"
 
-#ifdef XPAR_INTC_0_DEVICE_ID
-#include "xintc.h"
-#else
-#include "xscugic.h"
-#endif
-
-
+/************************** Constant Definitions *****************************/
 
 /*
  * The following constants map to the XPAR parameters created in the
  * xparameters.h file. They are defined here such that a user can easily
  * change all the needed parameters in one place.
  */
-#ifdef XPAR_INTC_0_DEVICE_ID
-#define INTC		XIntc
-#define UART_DEVICE_ID		XPAR_XUARTPS_0_DEVICE_ID
-#define INTC_DEVICE_ID		XPAR_INTC_0_DEVICE_ID
-#define UART_INT_IRQ_ID		XPAR_INTC_0_UARTPS_0_VEC_ID
-#else
-#define INTC		XScuGic
-#define UART_DEVICE_ID		XPAR_XUARTPS_0_DEVICE_ID
-#define INTC_DEVICE_ID		XPAR_SCUGIC_SINGLE_DEVICE_ID
-#define UART_INT_IRQ_ID		XPAR_XUARTPS_1_INTR
-#endif
+#define UART_DEVICE_ID              XPAR_XUARTPS_0_DEVICE_ID
+
 /*
  * The following constant controls the length of the buffers to be sent
- * and received with the UART,
+ * and received with the device, this constant must be 32 bytes or less since
+ * only as much as FIFO size data can be sent or received in polled mode.
  */
-#define TEST_BUFFER_SIZE	100
+#define TEST_BUFFER_SIZE 32
 
-
-static int SetupInterruptSystem(INTC *IntcInstancePtr,
-				XUartPs *UartInstancePtr,
-				u16 UartIntrId);
-
-void Handler(void *CallBackRef, u32 Event, unsigned int EventData);
-
-XUartPs UartPs	;		/* Instance of the UART Device */
-INTC InterruptController;	/* Instance of the Interrupt Controller */
+XUartPs Uart_PS;		/* Instance of the UART Device */
 
 /*
  * The following buffers are used in this example to send and receive data
@@ -59,343 +34,96 @@ INTC InterruptController;	/* Instance of the Interrupt Controller */
 static u8 SendBuffer[TEST_BUFFER_SIZE];	/* Buffer for Transmitting Data */
 static u8 RecvBuffer[TEST_BUFFER_SIZE];	/* Buffer for Receiving Data */
 
-/*
- * The following counters are used to determine when the entire buffer has
- * been sent and received.
- */
-volatile int TotalReceivedCount;
-volatile int TotalSentCount;
-int TotalErrorCount;
-
-
-int main(){
-
+int main(void)
+{
 	int Status;
+
 	XUartPs_Config *Config;
-	int Index;
-	u32 IntrMask;
-	int BadByteCount = 0;
-
-	u16 DeviceId = UART_DEVICE_ID;
-
-	/*if (XGetPlatform_Info() == XPLAT_ZYNQ_ULTRA_MP) {
-#ifdef XPAR_XUARTPS_1_DEVICE_ID
-		DeviceId = XPAR_XUARTPS_1_DEVICE_ID;
-#endif
-	}
-*/
+	unsigned int SentCount;
+	unsigned int ReceivedCount;
+	u16 Index;
+	u32 LoopCount = 0;
 
 	/*
-	 * Initialize the UART driver so that it's ready to use
+	 * Initialize the UART driver so that it's ready to use.
 	 * Look up the configuration in the config table, then initialize it.
 	 */
-	Config = XUartPs_LookupConfig(DeviceId);
+	Config = XUartPs_LookupConfig(UART_DEVICE_ID);
 	if (NULL == Config) {
 		return XST_FAILURE;
 	}
 
-	Status = XUartPs_CfgInitialize(&UartPs, Config, Config->BaseAddress);
+	Status = XUartPs_CfgInitialize(&Uart_PS, Config, Config->BaseAddress);
 	if (Status != XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 
-	/* Check hardware build */
-	Status = XUartPs_SelfTest(&UartPs);
+	/* Check hardware build. */
+	Status = XUartPs_SelfTest(&Uart_PS);
 	if (Status != XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 
-	/*
-	 * Connect the UART to the interrupt subsystem such that interrupts
-	 * can occur. This function is application specific.
-	 */
-	Status = SetupInterruptSystem(&InterruptController, &UartPs, UART_INT_IRQ_ID);
-	if (Status != XST_SUCCESS) {
-		return XST_FAILURE;
-	}
-
-	/*
-	 * Setup the handlers for the UART that will be called from the
-	 * interrupt context when data has been sent and received, specify
-	 * a pointer to the UART driver instance as the callback reference
-	 * so the handlers are able to access the instance data
-	 */
-	XUartPs_SetHandler(&UartPs, (XUartPs_Handler)Handler, &UartPs);
-
-	/*
-	 * Enable the interrupt of the UART so interrupts will occur, setup
-	 * a local loopback so data that is sent will be received.
-	 */
-	IntrMask =
-		XUARTPS_IXR_TOUT | XUARTPS_IXR_PARITY | XUARTPS_IXR_FRAMING |
-		XUARTPS_IXR_OVER | XUARTPS_IXR_TXEMPTY | XUARTPS_IXR_RXFULL |
-		XUARTPS_IXR_RXOVR;
-
-	if (UartPs.Platform == XPLAT_ZYNQ_ULTRA_MP) {
-		IntrMask |= XUARTPS_IXR_RBRK;
-	}
-
-	XUartPs_SetInterruptMask(&UartPs, IntrMask);
-
-	XUartPs_SetOperMode(&UartPs, XUARTPS_OPER_MODE_NORMAL);
-
-	/*
-	 * Set the receiver timeout. If it is not set, and the last few bytes
-	 * of data do not trigger the over-water or full interrupt, the bytes
-	 * will not be received. By default it is disabled.
-	 *
-	 * The setting of 8 will timeout after 8 x 4 = 32 character times.
-	 * Increase the time out value if baud rate is high, decrease it if
-	 * baud rate is low.
-	 */
-	XUartPs_SetRecvTimeout(&UartPs, 8);
+	/* Use local loopback mode. */
+	//XUartPs_SetOperMode(&Uart_PS, XUARTPS_OPER_MODE_LOCAL_LOOP);
+	//XUartPs_SetOperMode(&Uart_PS, XUARTPS_OPER_MODE_NORMAL);
 
 
 	/*
-	 * Initialize the send buffer bytes with a pattern and the
-	 * the receive buffer bytes to zero to allow the receive data to be
-	 * verified
+	 * Initialize the send buffer bytes with a pattern and zero out
+	 * the receive buffer.
 	 */
 	for (Index = 0; Index < TEST_BUFFER_SIZE; Index++) {
-
-		SendBuffer[Index] = (Index % 26) + 'A';
-
+		SendBuffer[Index] = '0' + Index;
 		RecvBuffer[Index] = 0;
 	}
-	xil_printf((char8 *)SendBuffer);
-	/*
-	 * Start receiving data before sending it since there is a loopback,
-	 * ignoring the number of bytes received as the return value since we
-	 * know it will be zero
-	 */
-	XUartPs_Send(&UartPs, SendBuffer, TEST_BUFFER_SIZE);
-
-	XUartPs_Recv(&UartPs, RecvBuffer, TEST_BUFFER_SIZE);
-	/*
-	 * Send the buffer using the UART and ignore the number of bytes sent
-	 * as the return value since we are using it in interrupt mode.
-	 */
-
-
-	/*
-	 * Wait for the entire buffer to be received, letting the interrupt
-	 * processing work in the background, this function may get locked
-	 * up in this loop if the interrupts are not working correctly.
-	 */
-
-	while (1) {
-		if ((TotalSentCount == TEST_BUFFER_SIZE) &&
-		    (TotalReceivedCount == TEST_BUFFER_SIZE)) {
-			break;
-		}
+	xil_printf("send buffer is: ");
+	xil_printf((char8 *) SendBuffer);
+	xil_printf("\r\n");
+	/* Block sending the buffer. */
+	SentCount = XUartPs_Send(&Uart_PS, SendBuffer, TEST_BUFFER_SIZE);
+	if (SentCount != TEST_BUFFER_SIZE) {
+		return XST_FAILURE;
 	}
 
-	/* Verify the entire receive buffer was successfully received */
+	/*
+	 * Wait while the UART is sending the data so that we are guaranteed
+	 * to get the data the 1st time we call receive, otherwise this function
+	 * may enter receive before the data has arrived
+	 */
+	while (XUartPs_IsSending(&Uart_PS)) {
+		LoopCount++;
+	}
+
+
+
+
+	/* Block receiving the buffer. */
+	ReceivedCount = 0;
+	while (ReceivedCount < TEST_BUFFER_SIZE) {
+		ReceivedCount +=
+			XUartPs_Recv(&Uart_PS, &RecvBuffer[ReceivedCount],
+					  (TEST_BUFFER_SIZE - ReceivedCount));
+	}
+
+	xil_printf("RecvBuffer is: ");
+	xil_printf((char8 *) RecvBuffer);
+	xil_printf("\r\n");
+	/*
+	 * Check the receive buffer against the send buffer and verify the
+	 * data was correctly received
+	 */
 	for (Index = 0; Index < TEST_BUFFER_SIZE; Index++) {
-		if (RecvBuffer[Index] != SendBuffer[Index]) {
-			BadByteCount++;
+		if (SendBuffer[Index] != RecvBuffer[Index]) {
+			return XST_FAILURE;
 		}
 	}
+	xil_printf("im here\r\n");
 
-	//xil_printf("im here\r\n");
-	xil_printf("BadByteCount:\r\n");
-	xil_printf("BadByteCount: %d",BadByteCount);
-	/* Set the UART in Normal Mode */
-	XUartPs_SetOperMode(&UartPs, XUARTPS_OPER_MODE_NORMAL);
-
-
-	/* If any bytes were not correct, return an error */
-	if (BadByteCount != 0) {
-		return XST_FAILURE;
-	}
-
-	xil_printf("im here1\r\n");
-
-	return 0;
-}
-
-
-
-/***************************************************************************/
-void Handler(void *CallBackRef, u32 Event, unsigned int EventData)
-{
-	/* All of the data has been sent */
-	if (Event == XUARTPS_EVENT_SENT_DATA) {
-		TotalSentCount = EventData;
-	}
-
-	/* All of the data has been received */
-	if (Event == XUARTPS_EVENT_RECV_DATA) {
-		TotalReceivedCount = EventData;
-	}
-
-	/*
-	 * Data was received, but not the expected number of bytes, a
-	 * timeout just indicates the data stopped for 8 character times
-	 */
-	if (Event == XUARTPS_EVENT_RECV_TOUT) {
-		TotalReceivedCount = EventData;
-	}
-
-	/*
-	 * Data was received with an error, keep the data but determine
-	 * what kind of errors occurred
-	 */
-	if (Event == XUARTPS_EVENT_RECV_ERROR) {
-		TotalReceivedCount = EventData;
-		TotalErrorCount++;
-	}
-
-	/*
-	 * Data was received with an parity or frame or break error, keep the data
-	 * but determine what kind of errors occurred. Specific to Zynq Ultrascale+
-	 * MP.
-	 */
-	if (Event == XUARTPS_EVENT_PARE_FRAME_BRKE) {
-		TotalReceivedCount = EventData;
-		TotalErrorCount++;
-	}
-
-	/*
-	 * Data was received with an overrun error, keep the data but determine
-	 * what kind of errors occurred. Specific to Zynq Ultrascale+ MP.
-	 */
-	if (Event == XUARTPS_EVENT_RECV_ORERR) {
-		TotalReceivedCount = EventData;
-		TotalErrorCount++;
-	}
-}
-
-
-/*****************************************************************************/
-/**
-*
-* This function sets up the interrupt system so interrupts can occur for the
-* Uart. This function is application-specific. The user should modify this
-* function to fit the application.
-*
-* @param	IntcInstancePtr is a pointer to the instance of the INTC.
-* @param	UartInstancePtr contains a pointer to the instance of the UART
-*		driver which is going to be connected to the interrupt
-*		controller.
-* @param	UartIntrId is the interrupt Id and is typically
-*		XPAR_<UARTPS_instance>_INTR value from xparameters.h.
-*
-* @return	XST_SUCCESS if successful, otherwise XST_FAILURE.
-*
-* @note		None.
-*
-****************************************************************************/
-static int SetupInterruptSystem(INTC *IntcInstancePtr,
-				XUartPs *UartInstancePtr,
-				u16 UartIntrId)
-{
-	int Status;
-
-#ifdef XPAR_INTC_0_DEVICE_ID
-#ifndef TESTAPP_GEN
-	/*
-	 * Initialize the interrupt controller driver so that it's ready to
-	 * use.
-	 */
-	Status = XIntc_Initialize(IntcInstancePtr, INTC_DEVICE_ID);
-	if (Status != XST_SUCCESS) {
-		return XST_FAILURE;
-	}
-#endif
-	/*
-	 * Connect the handler that will be called when an interrupt
-	 * for the device occurs, the handler defined above performs the
-	 * specific interrupt processing for the device.
-	 */
-	Status = XIntc_Connect(IntcInstancePtr, UartIntrId,
-		(XInterruptHandler) XUartPs_InterruptHandler, UartInstancePtr);
-	if (Status != XST_SUCCESS) {
-		return XST_FAILURE;
-	}
-
-#ifndef TESTAPP_GEN
-	/*
-	 * Start the interrupt controller so interrupts are enabled for all
-	 * devices that cause interrupts.
-	 */
-	Status = XIntc_Start(IntcInstancePtr, XIN_REAL_MODE);
-	if (Status != XST_SUCCESS) {
-		return XST_FAILURE;
-	}
-#endif
-	/*
-	 * Enable the interrupt for uart
-	 */
-	XIntc_Enable(IntcInstancePtr, UartIntrId);
-
-	#ifndef TESTAPP_GEN
-	/*
-	 * Initialize the exception table.
-	 */
-	Xil_ExceptionInit();
-
-	/*
-	 * Register the interrupt controller handler with the exception table.
-	 */
-	Xil_ExceptionRegisterHandler(XIL_EXCEPTION_ID_INT,
-				(Xil_ExceptionHandler) XIntc_InterruptHandler,
-				IntcInstancePtr);
-	#endif
-#else
-#ifndef TESTAPP_GEN
-	XScuGic_Config *IntcConfig; /* Config for interrupt controller */
-
-	/* Initialize the interrupt controller driver */
-	IntcConfig = XScuGic_LookupConfig(INTC_DEVICE_ID);
-	if (NULL == IntcConfig) {
-		return XST_FAILURE;
-	}
-
-	Status = XScuGic_CfgInitialize(IntcInstancePtr, IntcConfig,
-					IntcConfig->CpuBaseAddress);
-	if (Status != XST_SUCCESS) {
-		return XST_FAILURE;
-	}
-
-	/*
-	 * Connect the interrupt controller interrupt handler to the
-	 * hardware interrupt handling logic in the processor.
-	 */
-	Xil_ExceptionRegisterHandler(XIL_EXCEPTION_ID_INT,
-				(Xil_ExceptionHandler) XScuGic_InterruptHandler,
-				IntcInstancePtr);
-#endif
-
-	/*
-	 * Connect a device driver handler that will be called when an
-	 * interrupt for the device occurs, the device driver handler
-	 * performs the specific interrupt processing for the device
-	 */
-	Status = XScuGic_Connect(IntcInstancePtr, UartIntrId,
-				  (Xil_ExceptionHandler) XUartPs_InterruptHandler,
-				  (void *) UartInstancePtr);
-	if (Status != XST_SUCCESS) {
-		return XST_FAILURE;
-	}
-
-	/* Enable the interrupt for the device */
-	XScuGic_Enable(IntcInstancePtr, UartIntrId);
-
-#endif
-#ifndef TESTAPP_GEN
-	/* Enable interrupts */
-	 Xil_ExceptionEnable();
-#endif
+	/* Restore to normal mode. */
+	XUartPs_SetOperMode(&Uart_PS, XUARTPS_OPER_MODE_NORMAL);
 
 	return XST_SUCCESS;
+
+
 }
-
-
-
-
-
-
-
-
-
